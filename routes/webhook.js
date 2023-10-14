0bf61a4650a7b804a2bcc7db5724b4f977294237
Michael Rasolo <rasolon.michael@gmail.com>
2023-10-15 01:36:04 +0200

    teamInfo webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,10 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const fetch = require("node-fetch"); 
+const teamInfo = require("../controllers/webhookControllers"); 
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-console.log("Webhook route");
-res.send('Webhook route')
+/* WEBHOOK ROUTE */
+router.post("/", function (req, res) {
+
+// Call function for teamInfo intent
+  teamInfo(req.body.queryResult.parameters.teams) // Teams entity
+  .then((fulfillmentMessages) => {
+    
+      res.send(fulfillmentMessages);
+    })
+    .catch((error) => {
+      console.error("Error fetching team info:", error);
+      // Handle the error and send an error response if needed.
+      res.status(500).send("Internal Server Error");
+    });
 });
+
+
 
 module.exports = router;

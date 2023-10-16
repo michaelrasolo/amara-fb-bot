e256261775f5dac589f13e175a3f63af0d26985e
Michael Rasolo <rasolon.michael@gmail.com>
2023-10-16 02:07:45 +0200

    webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,28 +1,44 @@
-var express = require("express");
-var router = express.Router();
-const teamInfo = require("../controllers/teamInfo"); 
-const teamStadium = require("../controllers/teamStadium"); 
-const teamRank = require("../controllers/teamRank"); 
-const teamNextGame = require("../controllers/teamNextGame"); 
-const teamLastGame = require("../controllers/teamLastGame"); 
-const teamPlayers = require("../controllers/teamPlayers"); 
+const express = require("express");
+const router = express.Router();
+const teamInfo = require("../controllers/teamInfo");
+const teamStadium = require("../controllers/teamStadium");
+const teamRank = require("../controllers/teamRank");
+const teamNextGame = require("../controllers/teamNextGame");
+const teamLastGame = require("../controllers/teamLastGame");
+const teamPlayers = require("../controllers/teamPlayers");
+
+// All functions in an object
+const intentFunctions = {
+  teamInfo: teamInfo,
+  teamStadium: teamStadium,
+  teamRank: teamRank,
+  teamNextGame: teamNextGame,
+  teamLastGame: teamLastGame,
+  teamPlayers: teamPlayers,
+};
 
 /* WEBHOOK ROUTE */
 router.post("/", function (req, res) {
-console.log("intent",req.body.queryResult.intent.displayName);
-// Call function for teamInfo intent
-teamPlayers(req.body.queryResult.parameters.teams) // Teams entity
-  .then((fulfillmentMessages) => {
-    
-      res.send(fulfillmentMessages);
-    })
-    .catch((error) => {
-      console.error("Error fetching team info:", error);
-      // Handle the error and send an error response if needed.
-      res.status(500).send("Internal Server Error");
-    });
+  const intent = req.body.queryResult.intent.displayName.split(' ')[0];
+  const team = req.body.queryResult.parameters.teams;
+  console.log("intent", intent);
+
+  // if the intent name exists, call the corresponding function
+  if (intentFunctions[intent]) {
+    intentFunctions[intent](team) 
+      .then((fulfillmentMessages) => {
+        res.send(fulfillmentMessages);
+      })
+      .catch((error) => {
+        console.error("Error fetching team info:", error);
+        // Handle the error and send an error response if needed.
+        res.status(500).send("Internal Server Error");
+      });
+  } else {
+    // intent name is not recognized
+    console.error("Unknown intent:", intent);
+    res.status(400).send("Bad Request");
+  }
 });
-
-
 
 module.exports = router;
